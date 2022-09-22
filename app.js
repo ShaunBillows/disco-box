dda5cd39738b371fcea092748774b716837230bd
ShaunBillows <billows.shaun@gmail.com>
2022-09-22 23:06:17 +0100

    mp3 versions added

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  app(126, 9, "/sounds");
+  app(126, 13, "/sounds", "mp3");
 });
 
-const app = (bpm, instrumentNumber, path) => {
+const app = (bpm, instrumentNumber, path, fileType) => {
   const beatInterval = ((60 * 1000) / bpm) * 16; // miliseconds per 4 bars
   let startTime = 0;
 
   // initialise instruments
   const instruments = new Array(instrumentNumber)
     .fill(0)
-    .map((x, i) => new Instrument(`.pad-${i + 1}`, `${path}/${i + 1}.wav`));
+    .map((x, i) => new Instrument(`.pad-${i + 1}`, `${path}/${i + 1}.${fileType}`));
 
   instruments.forEach((instrument) => {
     instrument.pad.addEventListener("click", () => {
-      instrument.setIsPlaying();
-      instrument.setPadColor();
       setTimeout(() => {
         instrument.isPlaying ? instrument.playAudio() : instrument.pauseAudio();
       }, quantise(instruments));
+      instrument.setIsPlaying();
+      instrument.setPadColor();
     });
   });
 
@@ -26,6 +26,7 @@ const app = (bpm, instrumentNumber, path) => {
     if (instruments.some((x) => x.isPlaying)) {
       return beatInterval - ((Date.now() - startTime) % beatInterval);
     } else {
+      instruments.forEach((x) => console.log(x.isPlaying) )
       startTime = Date.now();
       return 0;
     }
